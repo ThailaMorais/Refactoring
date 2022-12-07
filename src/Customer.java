a4e66b74659d787159e52803f532aafd835961aa
Thaila Morais <thailamorais97@gmail.com>
2022-12-07 19:13:31 -0300

    Commit 5

--- src/Customer.java
+++ src/Customer.java
@@ -23,11 +23,7 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
-        Rental each = (Rental) rentals.nextElement();       
-        thisAmount = each.getCharge();
-
-        
+    	 Rental each = (Rental) rentals.nextElement();        
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -35,10 +31,10 @@ public class Customer {
         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
             each.getDaysRented() > 1) frequentRenterPoints ++;
 
-        //show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+        // show figures for this rental
+        result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf
+           (each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
      //add footer lines
